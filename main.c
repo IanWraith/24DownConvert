9db18654f5cad4312c3c8b00dc4dc48d736223f1
IanW <ianwraith@gmail.com>
2019-06-13 11:14:37 +0100

    Data transfer to the ADF4350 now works.

--- main.c
+++ main.c
@@ -2,16 +2,17 @@
 #include "regs.h"
 
 // PA01 - Indicator LED
+// PA02 - ADF4350 LE
 // PA04 - NSS
-// PA05 - SCK
+// PA05 - SCK to ADF4350 CLK
 // PA06 - MISO
-// PA07 - MOSI
+// PA07 - MOSI to ADF4350 DATA
 // PA08 - Test Push Switch
-// PA11 - LD
+// PA11 - ADF4350 LD
 
 void Delay(uint32_t);
-uint8_t SendReceiveSPIByte(uint32_t);
-void SendSPIData (uint8_t,uint32_t);
+uint16_t SendReceiveSPIData(uint16_t);
+void SendSPIDataADF4350 (uint8_t,uint32_t);
 
 int main(void)
 {
@@ -19,7 +20,7 @@ int main(void)
 	// Below are the ADF4350 settings for a 1 GHz -4dBm output
 	uint32_t ar0=0x00500000;
 	uint32_t ar1=0x08008011;
-	uint32_t ar2=0x00004e42;
+	uint32_t ar2=0x00004E42;
 	uint32_t ar3=0x000004b3;
 	uint32_t ar4=0x00ac8024;
 	uint32_t ar5=0x00580005;
@@ -35,12 +36,13 @@ int main(void)
 		
 	// I/O
 	// PA01 needs to be in push pull mode thus 0 turns on the LED and 1 turns it off (0x2) - Bits 
+	// PA02 needs to be in open drain mode (0x1) 
 	// PA04 (SPI NSS) needs to be in alternate push pull mode (0xa) 0b1010
 	// PA05 (SPI SCK) needs to be in alternate push pull mode (0xa)
 	// PA06 (SPI MISO) is a floating input (0x4)
 	// PA07 (SPI MOSI) needs to be in alternate push pull mode (0xa)
-	*(uint32_t *)GPIOA_BASE &= 0x0000ff0f;
-	*(uint32_t *)GPIOA_BASE |= 0xa4aa0020;
+	*(uint32_t *)GPIOA_BASE &= 0x0000f00f;
+	*(uint32_t *)GPIOA_BASE |= 0xa4aa0120;
 	// PA08 is a push switch input
 	// PA11 is a floating input
 	*(uint32_t *)(GPIOA_BASE + 0x04) &= 0xffff0ff0;
@@ -55,7 +57,7 @@ int main(void)
 	// 14 BIDIOE 0 - output enabled
 	// 13 CRCEN 0 - CRC disabled
 	// 12 CRCNEXT 0 - No CRC phase
-	// 11 DFF 0 - 8 bit data frame
+	// 11 DFF 1 - 16 bit data frame
 	// 10 RXONLY 0 - Full duplex
 	// 09 SSM 0 - Software slave management enabled
 	// 08 SSI 0
@@ -66,25 +68,24 @@ int main(void)
 	// 01 CPOL 0 - Clock to zero when idle
 	// 00 CPHA 0 - First clock transition is the data edge
 	*(uint32_t *)(SPI1_BASE) &= 0xffff0000;
-	*(uint32_t *)(SPI1_BASE) |= BIT_6 | BIT_5 | BIT_4 | BIT_3 | BIT_2;
-								
+	*(uint32_t *)(SPI1_BASE) |= BIT_11 | BIT_6 | BIT_5 | BIT_4 | BIT_3 | BIT_2;
+	
+	// Set PA02 (LE) low
+	*(uint32_t *)(GPIOA_BASE + 0x14) = BIT_2;
+	
 	// Flash the LED to show everything works
 	*(uint32_t *)(GPIOA_BASE + 0x14) = BIT_1;
 	Delay(500000);
 	*(uint32_t *)(GPIOA_BASE + 0x10) = BIT_1;
 	Delay(100000);
 		
-		
-	while (1)
-	{
-	// Send the setup data to the synth board
-	SendSPIData(0,ar0);
-	SendSPIData(1,ar1);
-	SendSPIData(2,ar2);
-	SendSPIData(3,ar3);
-	SendSPIData(4,ar4);
-	SendSPIData(5,ar5);
-	}
+	// Send the setup data to the ADF4350
+	SendSPIDataADF4350(5,ar5);
+	SendSPIDataADF4350(4,ar4);
+	SendSPIDataADF4350(3,ar3);
+	SendSPIDataADF4350(2,ar2);
+	SendSPIDataADF4350(1,ar1);
+	SendSPIDataADF4350(0,ar0);
 	
 	// Go into an endless loop waiting for frequency lock to be achieved
 	while(1)
@@ -109,14 +110,14 @@ void Delay(uint32_t tmax)
 		}
 }
 
-uint8_t SendReceiveSPIByte(uint32_t value)
+// Send a 16 bit word via SPI1 and receive a 16 bit word at the same time
+uint16_t SendReceiveSPIData(uint16_t value)
 {
 	uint8_t lout = 0;
-	// Put the 8 bits to be sent into the SPI data register
+	// Put the 16 bits to be sent into the SPI data register
 	*(uint32_t *)(SPI1_BASE + 0x0c) = value;
-	// Read a byte
-	uint8_t inbyte = *(uint32_t *)(SPI1_BASE + 0x0c);
-	
+	// Read in a 16 bit frame
+	uint16_t inbyte = *(uint32_t *)(SPI1_BASE + 0x0c);
 	// Loop waiting for the the SPI TXE flag to go high
 	while (lout == 0)
 	{
@@ -124,14 +125,39 @@ uint8_t SendReceiveSPIByte(uint32_t value)
 		uint32_t statusSPI = *(uint32_t *)(SPI1_BASE + 0x08);
 		// Just bit 1 the transmit buffer empty flag - wait for this to go high then leave this function
 		statusSPI &= BIT_1;
+		// The condition has been met so signal to get out of this loop
 		if (statusSPI == BIT_1) lout = 1;
 	}
-	
 	return inbyte;
 }
 
-void SendSPIData (uint8_t regNo,uint32_t outval)
+// Send a 29 bit register value plus 3 control bits to the ADF4350
+void SendSPIDataADF4350 (uint8_t regNo,uint32_t outval)
 {
-	SendReceiveSPIByte(0xa0);
+	// Shift the data being sent 3 bits to the left and add the register number
+	uint32_t sendword = (outval << 3) + regNo;
+	// This then needs splitting into 2 x 16 bit words
+    uint16_t highWord = (sendword & 0xffff0000) >> 16;
+	uint16_t lowWord = sendword & 0x0000ffff;
+	// Send these to the ADF4350 via SPI
+	SendReceiveSPIData (highWord);
+	SendReceiveSPIData (lowWord);
+	uint8_t lout = 0;
+	// Loop waiting for the the SPI BSY flag to go low
+	while (lout == 0)
+	{
+		// Read the SPI status register
+		uint32_t statusSPI = *(uint32_t *)(SPI1_BASE + 0x08);
+		// Just bit 7 the SPI BSY flag - wait for this to go low then leave this function
+		statusSPI &= BIT_7;
+		// The condition has been met so signal to get out of this loop
+		if (statusSPI == 0) lout = 1;
+	}
+	// Take LE high to load the data into the register
+	*(uint32_t *)(GPIOA_BASE + 0x10) = BIT_2;
+	// Short delay while LE is high (minimum of 20ns)
+	Delay(50);
+	// Take LE low again
+	*(uint32_t *)(GPIOA_BASE + 0x14) = BIT_2;
 }
 
